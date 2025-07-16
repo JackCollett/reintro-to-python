e7a7d32e9769a60a6b3901cd13688ce2e9320dd6
Jack Collett <jack.collett@live.co.uk>
2025-07-16 17:08:08 +0000

    Exercise_1 passed

--- 040_challenge_1_exercise.py
+++ 040_challenge_1_exercise.py
@@ -28,7 +28,40 @@ print("")
 print("Function: report_long_words")
 
 def report_long_words(words):
-  pass
+    long_words = get_long_words_only(words)
+    long_unhyphened_words = get_unhyphened_words(long_words)
+    long_unhyphened_shortened_words = shorten_very_long_words(long_unhyphened_words)
+    return f"These words are quite long: {long_unhyphened_shortened_words}"
+
+def get_long_words_only(words):
+    long_words = []
+    for word in words:
+      if len(word) > 9:
+        long_words.append(word)
+    return long_words
+
+def get_unhyphened_words(long_words):
+    long_unhyphened_words = []
+    for word in long_words:
+      if "-" not in word:
+        long_unhyphened_words.append(word)
+    return long_unhyphened_words
+
+def shorten_very_long_words(long_unhyphened_words):
+    long_unhyphened_shortened_words = ""
+    for word in long_unhyphened_words:
+      if len(word) > 15:
+        word = word[0:15] + "..."
+      long_unhyphened_shortened_words += word + ", "
+    return long_unhyphened_shortened_words[:-2]
+  
+print(shorten_very_long_words([
+      'hello',
+      'nonbiological',
+      'Kay',
+      'this-is-a-long-word',
+      'antidisestablishmentarianism'
+    ]))
 
 check_that_these_are_equal(
   report_long_words([
